d8be257f1fbb62bec9d20d1907c7f34fd299efa5
authentic-actor-8801 <xaviernational@aol.com>
2019-02-11 06:56:50 +0000

    Done.

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -1,128 +1,75 @@
-describe "#welcome" do
-  it "prints a welcome message to screen" do
-    expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
-    welcome
-  end
+def welcome
+  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
-describe "#deal_card" do
-  it "generates a random number between 1-11" do
-    20.times do 
-      card = deal_card
-      expect(card).to be > 0
-      expect(card).to be < 12
-    end
-  end
+def deal_card
+  # code #deal_card here
+  1 + rand(11)
 end
 
-describe "#display_card_total" do
-  it "accepts one argument, the card total" do
-    expect { display_card_total(7) }.to_not raise_error
-  end
-
-  it "prints the value of the cards to the screen" do
-    expect($stdout).to receive(:puts).with("Your cards add up to 8")
-    display_card_total(8)
-  end
-
-  it "does not hardcode the card total" do
-    expect($stdout).to receive(:puts).with("Your cards add up to 12")
-    display_card_total(12)
-  end
+def display_card_total(card_total)
+  # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
-
-describe "#prompt_user" do
-  it "gives instructions for hitting or staying" do
-    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-    prompt_user
-  end
+def prompt_user
+  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
-describe "#get_user_input" do
-  it "returns the value of a `gets.chomp` method" do
-    ["h", "s", "exit"].each do |string|
-      expect(self).to receive(:gets).and_return(string)
-      expect(get_user_input).to eq(string)
-    end
-  end
+def get_user_input
+  # code #get_user_input here
+  input = gets.chomp
 end
 
-describe "#end_game" do
-  it "prints apology, card total, and thank you message" do
-    expect($stdout).to receive(:puts).with("Sorry, you hit 27. Thanks for playing!")
-    end_game(27)
-  end
+def end_game(card_total)
+  # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-describe "#initial_round" do
-
-  it "calls on #deal_card twice and returns the sum" do
-    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-    expect(initial_round).to eq(12)
-  end
-
-  it "calls on the '#display_card_total' to print sum of cards" do
-    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-    expect($stdout).to receive(:puts).with(/Your cards add up to /)
-    initial_round
-  end
-
+def initial_round
+  # code #initial_round here
+  sum = deal_card
+  sum += deal_card
+  display_card_total(sum)
+  sum
 end
 
-describe "#hit?" do
-  before(:each) do
-    def get_user_input
-      "s"
-    end
+def hit?(card_total)
+  # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == "s"
+    input
+  elsif input == "h"
+    card_total += deal_card
+  else
+    invalid_command
   end
-
-  it "calls on #prompt_user then #get_user_input" do
-    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-    expect(self).to receive(:get_user_input).and_return("s")
-    hit?(7)
-  end
-
-  it "returns an integer which is the card total" do
-    expect(self).to receive(:get_user_input).and_return("s")
-    expect(hit?(20)).to eq(20)
-  end
-
-  it "does not deal another card if user types 's'" do
-    expect(self).to receive(:get_user_input).and_return("s")
-    expect(hit?(7)).to eq(7)
-  end
-
-  it "deals another card when user types 'h'" do
-    expect(self).to receive(:get_user_input).and_return("h")
-    expect(self).to receive(:deal_card).and_return(7)
-    expect(hit?(7)).to eq(14)
-  end
-
+  card_total
 end
 
-describe "#runner" do
+def invalid_command
+  # code invalid_command here
+  "Please enter a valid command"
+end
 
-  before(:each) do
-    def get_user_input
-      "h"
-    end 
+#####################################################
+# get every test to pass before coding runner below #
+#####################################################
+
+def runner
+  # code runner here
+  welcome
+  initial_round
+  counter = 0
+  counter += initial_round
+  until counter == 21
+    hand = 0
+    hand += hit?(initial_round)
+    display_card_total(initial_round)
+    counter += hand
   end
-
-  it "calls on the #welcome method, 
-  then on the #initial_round method, 
-  then calls #hit? and #display_card_total methods
-  -until- the card sum is greater than 21,
-  then calls on the #end_game method" do
-
-    expect(self).to receive(:deal_card).at_least(3).times.and_return(10)
-    expect(self).to receive(:get_user_input).and_return("h")
-
-    expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
-    expect($stdout).to receive(:puts).with("Your cards add up to 20")
-    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-    expect($stdout).to receive(:puts).with("Your cards add up to 30")
-    expect($stdout).to receive(:puts).with("Sorry, you hit 30. Thanks for playing!")
-    runner
-  end
+  end_game(hand)
 end
